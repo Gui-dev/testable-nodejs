f4be924db66450fd50cbba94635c1d6b27ce07c8
Gui Silva <adrianon10@gmail.com>
2023-04-05 03:42:15 -0300

    test: it should be able orders with an amount higher than 3000 should be marked as priority

--- src/create-order.spec.js
+++ src/create-order.spec.js
@@ -6,7 +6,7 @@ import { transport } from './mail/transport.js'
 
 describe('#Create Order', () => {
   mock.method(transport, 'sendMail', () => {
-    console.log('Enviou')
+    console.log('Enviou e-mail')
   })
 
   it('should be able to create a new order', async () => {
@@ -22,8 +22,26 @@ describe('#Create Order', () => {
         }
       }
     })
-    console.log(order.id)
     assert.ok(order.id)
+  })
+
+  it('should be able orders with an amount higher than 3000 should be marked as priority', async () => {
+    const order = await createOrder({
+      customerId: 'customer-fake-id',
+      amount: 4000
+    }, {
+      create (data) {
+        const id = 'order-fake-id'
+        return {
+          id,
+          customerId: data.customerId,
+          priority: data.isPriority,
+          amount: data.amount
+        }
+      }
+    })
+
+    assert.equal(order.priority, true)
   })
 })
 
